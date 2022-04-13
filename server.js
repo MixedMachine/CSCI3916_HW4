78f0b0f778f5abce38b5c01aad874430613b2bcf
Michael Marinez <80479622+MixedMachine@users.noreply.github.com>
2022-04-12 19:08:25 -0600

    debugging

--- server.js
+++ server.js
@@ -145,7 +145,8 @@ router.route('/movies')
             console.log("Getting reviews");
             // reviews is not null, so must check if it's true
             let movie = req.query;
-            movie.reviews = undefined;
+            // movie.reviews = undefined;
+            delete movie.reviews;
             console.log(movie);
             Movie.aggregate([
                 {
